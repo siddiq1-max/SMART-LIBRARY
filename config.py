2f77f92922b21f15a6ac70b93d1fc095230857f1
siddiq1-max <siddiqsaliyath@gmail.com>
2026-01-28 19:36:25 +0530

    Final deployment configuration

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-very-secret-smart-library'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///library.sqlite'
+    uri = os.environ.get('DATABASE_URL')
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///library.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
